d8cbd29aa06866e6804174bb0d4b9d77b4777212
KHAN IMRAN <imrankhanshakeel012@gmail.com>
2025-06-03 04:38:54 +0530

    made the UI of the app

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import AddTodo from './components/AddTodo';
+import { FaSun } from 'react-icons/fa';
+import Todos from './components/Todos';
+import TodoFilter from './components/TodoFilter';
+
+const App = () => {
+    return (
+        // <div className="transform absolute top-[50%] left-[50%] -translate-[50%] w-full px-6">
+        <div className=" max-w-3xl px-6 m-auto pt-16 pb-8">
+            <div className="flex justify-between items-center text-2xl font-sans ">
+                <h1 className="font-bold">T O D O' S</h1>
+                <FaSun className="cursor-pointer" />
+            </div>
+            <AddTodo />
+            <div className='border-1 dark:border-gray-700 border-gray-300 rounded shadow-lg'>
+                <Todos />
+                <Todos />
+                <Todos />
+                <Todos />
+            </div>
+            <TodoFilter />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
+
+const AddTodo = () => {
+    const [input, setInput] = useState('');
+
+    return (
+        <div className="mt-10 border-1 rounded dark:border-gray-700 border-gray-300 mb-6">
+            <form>
+                <div className="dark:bg-[#25273C] w-full text-xl p-4 rounded shadow-lg outline-0 flex justify-between gap-2">
+                    <input
+                        type="text"
+                        className="outline-0 w-full"
+                        placeholder="Create New Todo"
+                        onChange={e => setInput(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className={`cursor-pointer flex items-center gap-1 dark:bg-[#4d5066] text-white bg-gray-400 px-2 py-1 rounded text-sm ${input ? 'block' : 'hidden'}`}>
+                        <FaPlus className="text-sm" /> Add
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default AddTodo;
